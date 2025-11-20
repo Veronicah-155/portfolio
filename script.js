4e2c919253aceae08be51404827e0a8e4d7f69b6
Veronicah-155 <njugunaveraiconicah@gmail.com>
2025-11-20 15:04:46 +0300

    Added Project

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const projects = [
     title: "Anchor Global Website UI",
     description: "Figma design for corporate website with team and contact sections.",
     image: "assets/anchor.png",
-    type: "uiux",
+    type: "ui/ux",
     link: "https://www.figma.com/file/YourFigmaProjectID"
   },
   {
@@ -35,6 +35,13 @@ const projects = [
     image: "assets/ai-moderation.png",
     type: "AI Engineer",
     link: "https://github.com/Veronicah-155/ai-moderation-script"
+  },
+  {
+    title: "E-commerce Website",
+    description: "Kitenge Closet is an e-commerce platform showcasing African fashion, offering seamless browsing, detailed product views, and a smooth checkout experience.",
+    image: "assets/Kitenge.png",
+    type: "fullstack",
+    link: "https://github.com/Veronicah-155/Kitenge-Closet"
   }
 ];
 
@@ -45,19 +52,35 @@ projects.forEach((proj) => {
   let badge = "";
   if (proj.type === "fullstack") badge = "🌐 Fullstack Developer";
   else if (proj.type === "mobile") badge = "📱 Mobile Developer";
-  else if (proj.type === "uiux") badge = "🎨 UI/UX Designer";
+  else if (proj.type === "ui/ux") badge = "🎨 UI/UX Designer";
 
   const card = document.createElement("div");
-  card.innerHTML = `
-    <h3>${badge}</h3>
-    <img src="${proj.image}" alt="${proj.title}" class="rounded-lg mb-4 w-full h-48 object-cover">
-    <h4>${proj.title}</h4>
-    <p>${proj.description}</p>
-    <a href="${proj.link}" target="_blank" class="btn">View Project</a>
 
+  card.className =
+    "bg-[#013D0A] rounded-2xl shadow-lg p-10 border border-[#03A64A] text-center md:text-left";
+
+  card.innerHTML = `
+    <h2 class="text-3xl font-bold primary-color mb-2">${proj.title}</h2>
+    <p class="text-[#F0941F] text-lg">${proj.role1 || ""}</p>
+    <p class="text-[#F0941F] text-lg">${proj.role2 || ""}</p>
+    <p class="text-[#F0941F] text-lg">${proj.role3 || ""}</p>
+
+    <img src="${proj.image}"
+         alt="${proj.title}"
+         class="rounded-lg mt-6 mb-4 w-full h-48 object-cover">
+
+    <p class="mt-4 text-white">${proj.description}</p>
+
+    <a href="${proj.link}" 
+       target="_blank" 
+       class="inline-block mt-6 bg-[#03A64A] text-white px-5 py-2 rounded-lg hover:bg-[#028b3e] transition">
+       View Project
+    </a>
   `;
+
   projectsGrid.appendChild(card);
 });
+
 
 document.addEventListener("DOMContentLoaded", () => {
   type();
